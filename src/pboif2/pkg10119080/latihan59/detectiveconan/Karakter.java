d7be9f93cbfad2bfc1f8b27b2f708012bb5a71d2
PratamaVin7 <kevinpratamaoke@gmail.com>
2020-11-21 23:02:12 +0700

    First commit

---
+++ src/pboif2/pkg10119080/latihan59/detectiveconan/Karakter.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119080.latihan59.detectiveconan;
+
+/**
+ *
+ * @author
+ * NAMA     : Kevin Pratama Sinaga
+ * KELAS    : PBOIF2
+ * NIM      : 10119080
+ * Deskripsi Program : Program ini berisi program untuk menampilkan
+ * (OBJECT ORIENTED) Detective Conan
+ */
+public class Karakter extends Manusia {
+    private String role, kepribadian;
+    
+    public String getRole() {
+        return this.role;
+    }
+    
+    public void setRole(String role) {
+        this.role = role;
+    }
+    
+    public String getKepribadian() {
+        return this.kepribadian;
+    }
+    
+    public void setKepribadian(String kepribadian) {
+        this.kepribadian = kepribadian;
+    }
+    
+    public void tampilKarakter() {
+        System.out.println("Nama\t\t: " + super.getNama());
+        System.out.println("Umur\t\t: " + super.getUmur());
+        System.out.println("Kepribadian\t: " + getKepribadian());
+        System.out.println("Role\t\t: " + getRole());
+        System.out.println();
+    }
+    
+}
---
+++ src/pboif2/pkg10119080/latihan59/detectiveconan/Manusia.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119080.latihan59.detectiveconan;
+
+/**
+ *
+ * @author
+ * NAMA     : Kevin Pratama Sinaga
+ * KELAS    : PBOIF2
+ * NIM      : 10119080
+ * Deskripsi Program : Program ini berisi program untuk menampilkan
+ * (OBJECT ORIENTED) Detective Conan
+ */
+public class Manusia {
+    protected String nama;
+    protected int umur;
+    
+    public String getNama() {
+        return this.nama;
+    }
+    
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+    
+    public int getUmur() {
+        return this.umur;
+    }
+    
+    public void setUmur(int umur) {
+        this.umur = umur;
+    }
+    
+}
